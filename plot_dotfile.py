14782e799dd1373d72a6e5fc2cf7e68532421806
Yamila Omar Uni Ubuntu <yamila.omar@uni.lu>
2020-07-21 10:12:27 +0200

    updated plot_dotfile.py

--- plot_dotfile.py
+++ plot_dotfile.py
@@ -1,8 +1,9 @@
 # Manufacturing Entropy - Code for the article
 # Data: Kaggle competition - Bosch Manufacturing Data: train_date.csv file
+# Data used in this file: pre-processed ("data/clean_manufacturing_paths.txt" and "data/clean_manufacturing_edges.txt")
 # Yamila Mariel Omar
 # Date of original code: 15th April 2020
-# Date of code last modification: 15th April 2020
+# Date of code last modification: 21st July 2020
 
 if __name__ == "__main__":
     # Import needed modules
@@ -11,8 +12,15 @@ if __name__ == "__main__":
     from dotfile import DOT_File
 
     # Read necessary data
-    filename_edges = "data/manufacturing_edges.txt"
-    edges = GraphFile(filename_edges).read_edges_from_file()
+    filename_paths = "data/clean_manufacturing_paths.txt"
+    paths = GraphFile(filename_paths).read_paths_with_count()
+
+    # Generate edges with dummy source and sink
+    edges = dict()
+    for k,v in paths.items():
+        edges_list = [("source", k[0])] + [(i,j) for (i,j) in zip(k, k[1:])] + [(k[-1], "sink")]
+        for edge in edges_list:
+            edges[edge] = edges.get(edge, 0) + v
 
     # Process data
     G = Graph(edges)
@@ -23,33 +31,12 @@ if __name__ == "__main__":
     dot = DOT_File("manufacturing_network.dot", "Bosch", G, p)
     dot.save()
 
-
-    # Graph with only 0.1% or more in edges
-    # =====================================
-    total_items_produced = 0
-    for k,v in edges.items():
-        if k[0] == "i":
-            total_items_produced += v
-
-    print("Total items produced: ", total_items_produced)
-
-    reduced_edges = {k:v for k,v in edges.items() if v > 0.001 * total_items_produced}
-    reduced_G = Graph(reduced_edges)
-
-    dot = DOT_File("reduced_manufacturing_network.dot", "Bosch_reduced", reduced_G, p)
-    dot.save()
-
-    # Graph with node size related to entropy
-    # =======================================
-    C_H = GraphFile("results/manufacturing_entropy.txt").read_centrality_values_from_file()
-
-    dot = DOT_File("reduced_manufacturing_network_entropy.dot", "Bosch_entropy", reduced_G, C_H)
-    dot.save()
-
-
-    # Graph with node size related to scaled entropy
-    # ==============================================
-    C_H = GraphFile("results/manufacturing_entropy_scaled.txt").read_centrality_values_from_file()
-
-    dot = DOT_File("reduced_manufacturing_network_entropy_scaled.dot", "Bosch_entropy", reduced_G, C_H)
+    # Only above threshold
+    # ====================
+    number_of_manufactured_items = sum(list(paths.values()))
+    threshold = 0.005 * number_of_manufactured_items
+    edges = {k:v for k,v in edges.items() if v >= threshold}
+    G = Graph(edges)
+    p = {n: 1 for n in G.nodes}
+    dot = DOT_File("manufacturing_network_05_percent_threshold.dot", "Bosch", G, p)
     dot.save()
