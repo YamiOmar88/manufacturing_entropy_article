3689d6b891467ce2ff698d2e9e904a1cac37d969
Yamila Omar Uni Ubuntu <yamila.omar@uni.lu>
2020-07-21 10:12:57 +0200

    updated from_paths_to_entropy.py

--- from_paths_to_entropy.py
+++ from_paths_to_entropy.py
@@ -1,24 +1,19 @@
 # Manufacturing Entropy - Code for the article
-# Data: Kaggle competition - Bosch Manufacturing Data: train_date.csv file
+# Original Data: Kaggle competition - Bosch Manufacturing Data: train_date.csv file
+# Data used in this file: pre-processed ("data/clean_manufacturing_paths.txt" and "data/clean_manufacturing_edges.txt")
 # Yamila Mariel Omar
 # Date of original code: 29th October 2019
-# Date of code last modification: 15th April 2020
+# Date of code last modification: 21st July 2020
 
 
 
 # ===== Function definitions ============
 
-def remove_i_and_f(edges):
-    new_edges = dict()
-    for k,v in edges.items():
-        if 'i' in k:
-            continue
-        elif 'f' in k:
-            key = (k[0],k[0])
-            new_edges[key] = v
-        else:
-            new_edges[k] = v
-    return new_edges
+def add_self_loops(paths, edges):
+    for k,v in paths.items():
+        self_loop = (k[-1], k[-1])
+        edges[self_loop] = edges.get(self_loop, 0) + v
+    return edges
 
 # ===== End Function definitions =========
 
@@ -26,62 +21,43 @@ def remove_i_and_f(edges):
 
 if __name__ == "__main__":
     # Import needed modules
+    # =====================
     from graphfile import GraphFile
     from graph import Graph
-    from itertools import product
-    from math import log
+    import multiprocessing
     import datetime
 
-    # Read necessary data
-    filename_paths = "data/manufacturing_paths.txt"
-    filename_edges = "data/manufacturing_edges.txt"
+    # Read data: clean paths and clean edges
+    # ======================================
+    filename_paths = "data/clean_manufacturing_paths.txt"
+    filename_edges = "data/clean_manufacturing_edges.txt"
     edges = GraphFile(filename_edges).read_edges_from_file()
     paths = GraphFile(filename_paths).read_paths_with_count()
 
-    # Process data
-    edges = remove_i_and_f(edges)
+    # Generate graph from clean edges
+    # ===============================
+    edges = add_self_loops(paths, edges)
     G = Graph(edges)
 
     print("Number of nodes: ", len(G.nodes))
     print("Number of edges: ", len(G.edges.keys()))
 
-    # Process paths as needed
-    all_paths = {(i,j):[] for i,j in product(G.nodes,G.nodes)}
-    for path,count in paths.items():
-        for index in range(len(path)):
-            p = path[index:]
-            i,j = p[0], p[-1]
-            all_paths[(i,j)].append(p)
+    # =========================================================================
+    # CALCULATE ENTROPY
+    # =========================================================================
 
-    for k,v in all_paths.items():
-        v = set(v)
-        v = [list(x) for x in v]
-        all_paths[k] = v
-
-
-    # Find number of possible endings for each i
-    endings = {}
-    for n in G.nodes:
-        counter = 0
-        for k,v in all_paths.items():
-            if k[0] == n and len(v) > 0: counter += 1
-        endings[n] = counter
-
-
-    # Calculate the entropy
+    # Method 1: finding all paths in the graph
+    # ========================================
+    pool = multiprocessing.Pool()
     start = datetime.datetime.now()
-    G.all_paths = all_paths
-    C_H = dict()
-    for n in G.nodes:
-        node, entropy = G.get_node_entropy(n)
-        C_H[node] = (1/log(len(G.nodes), 2))*entropy
+    print("Start time: ", start)
+    r = pool.map(G.get_node_entropy, G.nodes)
     end = datetime.datetime.now()
-    print("\nStart time: ", start)
     print("End time: ", end)
-    print("Run time: ", end-start)
+    print("Run time: ", end - start)
+    pool.close()
+    pool.join()
 
-    entropy_scaled = {n:endings[n]*C_H[n] for n in G.nodes}
+    C_H = {i:C for (i,C) in r}
 
-    # Save results to file
-    GraphFile("results/manufacturing_entropy.txt").write_centrality_values_to_file(C_H)
-    GraphFile("results/manufacturing_entropy_scaled.txt").write_centrality_values_to_file(entropy_scaled)
+    GraphFile("results/entropy_method_1.txt").write_centrality_values_to_file(C_H)
