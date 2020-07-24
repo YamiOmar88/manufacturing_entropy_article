f41ea785f2317845a0d9e71d1fd8061b99efa09b
Yamila Omar Uni Ubuntu <yamila.omar@uni.lu>
2020-07-24 08:40:02 +0200

    worked on another method in script

--- from_paths_to_entropy.py
+++ from_paths_to_entropy.py
@@ -18,7 +18,16 @@ def add_self_loops(paths, edges):
 
 
 def calculate_entropy(G, p_ij):
-    ''' '''
+    '''Auxiliary function to calculate the entropy centrality without using the
+    class method in graph.py.
+    Input variables:
+    - G: graph (see graph.py for details)
+    - p_ij: dictionary of path probabilities. Keys are (i,j) node pairs, and
+    values are probabilities.
+    Output variables:
+    - C_H: dictionary of entropy centrality. Keys are nodes and values are their
+    entropy centrality.
+    '''
     from math import log
 
     C_H = {i:0 for i in G.nodes}
@@ -28,6 +37,28 @@ def calculate_entropy(G, p_ij):
                 C_H[i] = C_H[i] + p_ij[(i,j)] * log(p_ij[(i,j)], 2)
     C_H = {k:-v for k,v in C_H.items()}
     return C_H
+
+
+
+def find_all_subpaths_of_path(path):
+    '''
+    This function finds all subpaths of a path.
+    Input variables:
+    - path: tuple containing a path.
+    Output variables:
+    - subpaths: set of subpaths in path.
+
+    Example:
+    path = (1, 2, 3, 4)
+    >>> find_all_subpaths_of_path(path)
+    {(1, 2), (1, 2, 3, 4), (1,), (2,), (3,), (1, 2, 3), (4,), (2, 3), (2, 3, 4), (3, 4)}
+    '''
+    subpaths = set()
+    for i in range(len(path)):
+        for j in range(i+1, len(path)+1):
+            subpaths.add( path[i:j] )
+    return subpaths
+
 
 # ===== End Function definitions =========
 
@@ -125,6 +156,45 @@ if __name__ == "__main__":
         C_H_method4 = calculate_entropy(G, p_ij)
         GraphFile("results/entropy_method_4.txt").write_centrality_values_to_file(C_H_method4)
 
+
+    elif method == 5:
+        # Method 5: newly defined path probability (using manufacturing paths)
+        # ====================================================================
+        def path_prob(path):
+            i, j = path[0], path[-1]
+            product = 1
+            for node in path[:-1]:
+                T_k = G._transfer_probability(node, path)
+                product = product * T_k
+            return i, j, product
+
+
+
+        # Get all subpaths
+        pool = multiprocessing.Pool()
+        start = datetime.datetime.now()
+        print("Start time: ", start)
+        r = pool.map(find_all_subpaths_of_path, paths.keys())
+        end = datetime.datetime.now()
+        print("End time: ", end)
+        print("Run time: ", end - start)
+        pool.close()
+        pool.join()
+
+
+        p_ij = {(i,j):0 for i in G.nodes for j in G.nodes}
+        seen = set()
+        for result in r:
+            for path in result:
+                if path not in seen:
+                    i, j, prob = path_prob(path)
+                    p_ij[(i,j)] += prob
+                    seen.add(path)
+
+        print("Number of subpaths: ", len(seen))
+
+        C_H_method5 = calculate_entropy(G, p_ij)
+        GraphFile("results/entropy_method_5.txt").write_centrality_values_to_file(C_H_method5)
 
 
     else:
